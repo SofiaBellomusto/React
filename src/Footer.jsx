025ee13c4f3cd9846be3c385d173e07b412e8027
SofiaBellomusto <141052990+SofiaBellomusto@users.noreply.github.com>
2024-01-29 11:38:52 -0300

    primeros archivos

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+    return <div>
+        <h4 className={styles.title}>Este es el footer</h4>
+        <h5 className={styles.titleDos}>TitleDos</h5>
+    </div>
+}
---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar"
+
+export const Navbar = () => {
+    return <nav>
+        <ul>
+            <li>Productos</li>
+            <li>Servicios</li>
+            <li>Contacto</li>
+        </ul>
+    </nav>
+}
